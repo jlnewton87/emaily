3b98056ed9da9cf1234841fb207e1e8d61551475
Josh Newton <josh.newton@careerbuilder.com>
2018-02-17 11:14:10 -0500

    adding logout route

--- routes/auth.js
+++ routes/auth.js
@@ -7,6 +7,12 @@ module.exports = (app) => {
 
   app.get('/auth/google/callback', passport.authenticate('google'));
 
+  app.get('/api/logout', (req, res) => {
+    req.logout();
+    res.send({ success: true });
+  });
+
+
   app.get('/api/current_user', (req, res) => {
     res.send(req.user);
   });
