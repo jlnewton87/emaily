78d4f7ed0329e41b0d4ce0c285e0b562446e6ac9
Josh Newton <josh.newton@careerbuilder.com>
2018-02-17 10:59:26 -0500

    mongoose and auth __mostly__ setup

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const routes = require('./routes');
+const keys = require('./config/keys');
+const db = require('./config/db');
+require('./models/user');
+
+mongoose.connect(db.getConnectionString());
 
 const app = express();
+
+app.use(cookieSession({
+  maxAge: 30 * 24 * 60 * 60 * 1000,
+  keys: [keys.cookieKey]
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 routes(app).auth;
 
--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,18 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
+
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id)
+    .then(user => done(null, user));
+});
 
 passport.use(
   new GoogleStrategy({
@@ -8,9 +20,17 @@ passport.use(
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback'
   }, (accessToken, refreshToken, profile, done) => {
-    console.log(`Access Token: ${accessToken}`);
-    console.log(`Refresh Token: ${refreshToken}`);
-    console.log(`profile: ${JSON.stringify(profile)} ><>`);
+    User.findOne({ googleId: profile.id })
+      .then( user => {
+        if (user) {
+          done(null, user);
+        }
+        else {
+          new User({ googleId: profile.id })
+            .save()
+            .then( newUser => done(null, newUser) );
+        }
+      });
   })
 );
 
