948dc71a25ad56949bce2e76b44d46e541d67b9e
Josh Newton <josh.newton@careerbuilder.com>
2018-02-16 23:36:17 -0500

    get ready for heroku deployment

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const prettyConsole = require("./pretty-console");
 const app = express();
-const appPort = 5000;
+const appPort = process.env.PORT || 5000;
 
 app.get("/", (req, res) =>
   res.send({
